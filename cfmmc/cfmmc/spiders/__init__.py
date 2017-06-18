1800cfb1ad1cb75586cdd1f62f040016b9d17322
lamter <lamter.fu@gmail.com>
2017-06-18 09:42:28 +0800

    - add : 创建 Scrapy 目录

---
+++ cfmmc/cfmmc/spiders/__init__.py
@@ -0,0 +1,8 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+'''
+放置spider代码的目录。
+'''
